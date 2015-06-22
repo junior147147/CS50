77add60d51fc8d4485b3c17c620e00a90ceae3ba
junior147147 <junior147147@yahoo.com>
2015-06-22 17:32:15 -0600

    vigenere

--- Pset_2/vigenere.cpp
+++ Pset_2/vigenere.cpp
@@ -11,7 +11,6 @@
  * Ci = (Pi + Ki) % 26
  **************************************************************************/
 
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,14 +20,13 @@
 using namespace std;
 
 
+
 int main(int argc, char const *argv[])
 {
 
-
 /***************************************************************************
 Step 1 : Check validation of arguments input
-***************************************************************************/
-	
+**************************************************************************/
 	bool is_two_argc = false;
 	do{
 		if (argc != 2){ 
@@ -40,15 +38,17 @@ Step 1 : Check validation of arguments input
 			is_two_argc = true;
 			}
 		}while(!is_two_argc);
-	
+		
 
 /***************************************************************************
 Step 2: set key from characters to numbers (ex: a&A = 0  etcs)
-***************************************************************************/
+**************************************************************************/
 
-	//set input key to string key_char
-	string key_char = argv[1];
-	//size is from the begining of the input to the end, brilliant!!
+//set input key to string key_char
+string key_char = argv[1];
+//size is from the begining of the input to the end, brilliant!!
+std::vector<int> key_num(key_char.begin(), key_char.end());  
+
 	for (int i = 0; i < key_char.length(); i++){
 		if (isalpha(key_char[i])){
 			if (islower(key_char[i])){
@@ -62,39 +62,41 @@ Step 2: set key from characters to numbers (ex: a&A = 0  etcs)
 
 /***************************************************************************
 Step 3: Asking for message
-***************************************************************************/
+**************************************************************************/
 
-cout << "Please give me a string for encoding" << endl;
-getline(cin,sentence);
+
+	string sentence;
+	string output_sentence;
+	cout << "Please give me a string for encoding" << endl;
+	getline(cin,sentence);
+
 
 /***************************************************************************
 Step 4: Implementing encoding algorithm
-***************************************************************************/
+**************************************************************************/
 	
 	//key counters
 	int j = 0;
-	for(int i = 0; i < sentence.length(); i++){
-		//key counters wrap around and reset if its size is smaller than the encoded text
-		j = j%key_num.size();
 
-		//caecar algorithm with keys
+	for(int i = 0; i < sentence.length(); i++){
+		//key counters wrap around and reset if its size is smaller than the encoded message
+		j = j%key_num.size();
 		if (isalpha(sentence[i])){
-					if (islower(sentence[i])){
-					output_sentence[i] = (sentence[i] + key_num[j] - 'a') % 26 + 'a';
-					cout << output_sentence[i];
-					j++;
-				}
-				else{
-					output_sentence[i] = (sentence[i] + key_num[j] - 'A') % 26 + 'A';
-					cout << output_sentence[i];
-					j++;
-				}
+			if (islower(sentence[i])){
+				output_sentence[i] = (sentence[i] + key_num[j] - 'a') % 26 + 'a';
+				cout << output_sentence[i];
+				j++;
 			}
-		else{
-			cout << sentence[i];
+			else{
+				output_sentence[i] = (sentence[i] + key_num[j] - 'A') % 26 + 'A';
+				cout << output_sentence[i];
+				j++;
 			}
 		}
+		else cout << sentence[i];		
 	}
+
+}
 
 
 
