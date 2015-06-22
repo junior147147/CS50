01250e3da691c494ef48134ce32467d73a78233a
junior147147 <junior147147@yahoo.com>
2015-06-22 17:55:16 -0600

    Adding comments

--- Pset_2/caecar.cpp
+++ Pset_2/caecar.cpp
@@ -1,3 +1,23 @@
+/***********************************************************************************
+ * FILENAME: caecar.cpp
+ * 
+ * Mon 21 June, 3:00 PM, 2015
+ *
+ * Author: Yu Zhou
+ * 
+ * Pset 2 - 2 of 3
+ *
+ * Program encrypts messages using Caesar's cipher.
+ * Formula:	Ci = (Pi + K) % 26
+ * C1 = New, changed character
+ * P1 = First letter of original phrase
+ * K = Number the user provides (the key)
+ * 26 = Number of letters in the alphabet
+ *
+ * New = (Original + Key) % 26
+ ***********************************************************************************/
+
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +28,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 
+/***********************************************************************************
+Step 1 : Check validation of command-line arguments input
+
+* atoi: convert the character(s) to an integer 
+
+***********************************************************************************/
 	bool is_two_argc = false;
 	int k;
 	//Check validation of arguments input
@@ -23,23 +49,31 @@ int main(int argc, char const *argv[])
 			}
 		}while(!is_two_argc);
 		
-	
+/***********************************************************************************
+Step 2: Asking for message
 
-	//Prompt the user to input strings and k
+* make sure to use getline for a string
+
+***********************************************************************************/
+
 	string sentence;
 	cout << "Please input a string please!";
 	getline(cin,sentence);
 
 
-	//output with each alphabetical character rotated by argument "k" positions
-	//Cipher[i] = (i + k) % 26
+/***********************************************************************************
+Step 3: Implementing encoding algorithm
+
+* Output with each alphabetical character rotated by argument "k" positions
+* Cipher[i] = (i + k) % 26
+* 'a' = 97
+* 'A' = 65
+
+***********************************************************************************/
 
 	string output_sentence;
 
-
-
-	for (int i = 0; i <= sentence.length(); i++){
-		
+	for (int i = 0; i <= sentence.length(); i++){		
 		//print the character
 		if (isalpha(sentence[i])){
 			if (islower(sentence[i])){
@@ -51,13 +85,10 @@ int main(int argc, char const *argv[])
 				cout << output_sentence[i];
 				}
 			}
-		
 		//print the space
 		else {
 			cout << sentence[i];
 		}
 	}
-
-
 	return 0;
 }
