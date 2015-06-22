b9ff1f0e27db39ff1c471aadd6bc499fac0a1273
junior147147 <junior147147@yahoo.com>
2015-06-22 17:16:34 -0600

    vigenere

--- Pset_2/vigenere.cpp
+++ Pset_2/vigenere.cpp
@@ -30,17 +30,17 @@ int main(int argc, char const *argv[])
 /***************************************************************************
 Step 1 : Check validation of arguments input
 ***************************************************************************/
-	do{
-		if (argc != 2){ 
-			cout << "Don't forget to input your key with a number";
-			return 1;
-			}
+do{
+	if (argc != 2){ 
+		cout << "Don't forget to input your key with a number";
+		return 1;
+		}
 
-		else{
-			is_two_argc = true;
-			}
-		}while(!is_two_argc);
-		
+	else{
+		is_two_argc = true;
+		}
+	}while(!is_two_argc);
+	
 
 /***************************************************************************
 Step 2: set key from characters to numbers (ex: a&A = 0  etcs)
@@ -51,57 +51,54 @@ string key_char = argv[1];
 	//size is from the begining of the input to the end, brilliant!!
 std::vector<int> key_num(key_char.begin(), key_char.end());  
 
-	for (int i = 0; i < key_char.length(); i++){
-		if (isalpha(key_char[i])){
-			if (islower(key_char[i])){
-				key_num[i] = key_char[i] - 'a';
-			}
-			else{
-				key_num[i] = key_char[i] - 'A';
-			}
+for (int i = 0; i < key_char.length(); i++){
+	if (isalpha(key_char[i])){
+		if (islower(key_char[i])){
+			key_num[i] = key_char[i] - 'a';
+		}
+		else{
+			key_num[i] = key_char[i] - 'A';
 		}
 	}
+}
 
 /***************************************************************************
 Step 3: Asking for message
 ***************************************************************************/
 
-
-	string sentence;
-	string output_sentence;
-	cout << "Please give me a string for encoding" << endl;
-	getline(cin,sentence);
-
+string sentence;
+string output_sentence;
+cout << "Please give me a string for encoding" << endl;
+getline(cin,sentence);
 
 /***************************************************************************
 Step 4: Implementing encoding algorithm
 ***************************************************************************/
 	
 	//key counters
-	int j = 0;
+int j = 0;
 
-	for(int i = 0; i < sentence.length(); i++){
-		//key counters wrap around and reset if its size is smaller than the encoded text
-		j = j%key_num.size();
+for(int i = 0; i < sentence.length(); i++){
+	//key counters wrap around and reset if its size is smaller than the encoded text
+	j = j%key_num.size();
 
-		//caecar algorithm with keys
-		if (isalpha(sentence[i])){
-					if (islower(sentence[i])){
-					output_sentence[i] = (sentence[i] + key_num[j] - 'a') % 26 + 'a';
-					cout << output_sentence[i];
-					j++;
-				}
-				else{
-					output_sentence[i] = (sentence[i] + key_num[j] - 'A') % 26 + 'A';
-					cout << output_sentence[i];
-					j++;
-				}
+	//caecar algorithm with keys
+	if (isalpha(sentence[i])){
+				if (islower(sentence[i])){
+				output_sentence[i] = (sentence[i] + key_num[j] - 'a') % 26 + 'a';
+				cout << output_sentence[i];
+				j++;
 			}
-		else{
-			cout << sentence[i];
+			else{
+				output_sentence[i] = (sentence[i] + key_num[j] - 'A') % 26 + 'A';
+				cout << output_sentence[i];
+				j++;
+			}
+		}
+	else{
+		cout << sentence[i];
 		}
 	}
-
 }
 
 
